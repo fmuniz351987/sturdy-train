445ea1f3a9cca3ea4300f40b95656e57c2e925bf
felipe351987 <muniz.r.felipe@gmail.com>
2018-05-20 01:54:45 -0300

    Adicionado decodificador.c

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 gcc codificador.c basemath.c strascii.c -o codificador.out -lm -Wall -std=c11
-# gcc decodificador.c basemath.c strascii.c -o decodificador.out -lm -Wall -std=c11
+gcc decodificador.c basemath.c strascii.c -o decodificador.out -lm -Wall -std=c11
---
+++ decodificador.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h> // funcao atoi()
+#include <string.h>
+#include <math.h>
+
+#include "basemath.h"
+#include "strascii.h"
+
+#define MAX_STRING_SIZE 100
+#define INVALID_PARAMETERS 1
+#define VECTOR_TERMINATOR -1
+
+int main() {
+	return 0;
+}
